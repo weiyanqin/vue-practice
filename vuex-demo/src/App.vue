989b809ba3f062a1398cbe91128cb3d321e7a00e
weiyanqin <wanghaiming723@foxmail.com>
2018-11-06 23:03:29 +0800

    vuex-demo 通过属性传值的形式，根组件将数据传递给子组件

---
+++ vuex-demo/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ProductListOne v-bind:products="products"/>
+    <ProductListTwo v-bind:products="products"/>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/ProductListOne"
+import ProductListTwo from "./components/ProductListTwo"
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      products: [
+        {name: '马云', price: '200'},
+        {name: '马化腾', price: '140'},
+        {name: '马冬梅', price: '20'},
+        {name: '马蓉', price: '10'}
+      ]
+    }
+  },
+  components: { ProductListOne,ProductListTwo }
+}
+</script>
+
+<style>
+ body{
+   font-family: Ubuntu;
+   color: #555;
+ } 
+</style>
